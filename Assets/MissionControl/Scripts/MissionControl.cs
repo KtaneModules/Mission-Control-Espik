cbabf00ecbc933db6909c37078fc3e1a44602965
Espik <sbick23@outlook.com>
2022-10-30 22:53:09 -0500

    Jam Bomb - Part 1

--- Assets/MissionControl/Scripts/MissionControl.cs
+++ Assets/MissionControl/Scripts/MissionControl.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using UnityEngine;
 using KModkit;
 using System.Text.RegularExpressions;
+using Wawa.DDL;
 
 public class MissionControl : MonoBehaviour {
     public KMAudio Audio;
@@ -19,6 +20,9 @@ public class MissionControl : MonoBehaviour {
     public GameObject[] Cover;
     public GameObject[] PivotRight;
     public GameObject[] PivotLeft;
+
+    public Material PlanetMaterial;
+    public Material BorderMaterial;
 
 
     // Logging info
@@ -41,6 +45,7 @@ public class MissionControl : MonoBehaviour {
      * 2: Dead End (Small)
      * 3: Disconnected
      * 4: Wish
+     * 5: Precise Instability
      */
 
     // Mission specific variables
@@ -63,6 +68,42 @@ public class MissionControl : MonoBehaviour {
 
     private KMBombModule[] mystifiedModule = new KMBombModule[12];
     private Vector3[] mystifyScale = new Vector3[12];
+
+
+    private bool acceptingStrikes = false;
+    private bool readyToChange = false;
+    private int actualStrikes = 0;
+    private int bombStrikes = 0;
+    private int storedNumber = 1;
+    private float storedTime = 4800.0f;
+
+    private bool franticMode = false;
+    private bool freezeTimer = false;
+    private int displayedSecond = 30;
+    private int enteredSecond = 0;
+    private bool enteredTimerNumber = false;
+
+    private readonly int JAM_STRIKE_LIMIT = 4;
+    private readonly int JAM_MODULE_COUNT = 55;
+    private readonly float JAM_BOMB_TIME = 4800.0f;
+
+    private readonly string[] JAM_MODULES = { "3dTunnels", "AdjacentLettersModule", "atlantis", "bafflingBox", "boolMaze", "CheapCheckoutModule", "colorfulHexabuttons",
+        "ColourFlash", "CrazyTalk", "cruelModulo", "cucumberModule", "decimation", "DecolourFlashModule", "digitString", "DiscoloredSquaresModule", "GlitchedButtonModule",
+        "GrayButtonModule", "gyromaze", "HitmanModule", "KritHoldUps", "HumanResourcesModule", "identificationCrisis", "Indentation", "SCP2719", "jewelVault", "latinHypercube",
+        "Laundry", "Lean", "letteredHexabuttons", "mazematics", "meteor", "MssngvWls", "neptune", "NotBitmapsModule", "OnlyConnectModule", "PianoParadoxModule", "poetry", "rottenBeans",
+        "PasswordV2", "qSchlagDenBomb", "SetModule", "shapesAndColors", "shapeshift", "simonSelectsModule", "SimonShiftsModule", "simonStumbles", "ButtonV2", "stabilityModule",
+        "timeMachine", "undertunneling", "vigenereCipher", "widdershins", "X01", "zeroZero" };
+
+    private readonly string[] JAM_MODULE_NAMES = { "3D Tunnels", "Adjacent Letters", "Atlantis", "Baffling Box", "Boolean Maze", "Cheap Checkout", "Colorful Hexabuttons",
+        "Colour Flash", "Crazy Talk", "Cruel Modulo", "Cucumber", "Decimation", "Decolour Flash", "Digit String", "Discolored Squares", "The Glitched Button",
+        "The Gray Button", "Gyromaze", "Hitman", "Hold Ups", "Human Resources", "Identification Crisis", "Indentation", "Inside", "The Jewel Vault", "Latin Hypercube",
+        "Laundry", "LEAN!!!", "Lettered Hexabuttons", "Mazematics", "Meteor", "Mssngv Wls", "Neptune", "Not Bitmaps", "Only Connect", "Piano Paradox", "Poetry", "Rotten Beans",
+        "Safety Safe", "Schlag den Bomb", "S.E.T.", "Shapes and Colors", "Shape Shift", "Simon Selects", "Simon Shifts", "Simon Stumbles", "Square Button", "Stability",
+        "Time Machine", "Undertunneling", "Vigenère Cipher", "Widdershins", "X01", "Zero, Zero" };
+
+    private KMBombModule[] jamModule = new KMBombModule[54];
+    private Vector3[] jamModuleScale = new Vector3[54];
+    private Vector3 missionControlScale;
 
     // Mod settings
     private MissionControlSettings Settings;
@@ -119,6 +160,23 @@ public class MissionControl : MonoBehaviour {
             mode = 4;
             StartCoroutine(HideWishModules());
             break;
+        }
+
+        if (!missionFound && Bomb.GetSolvableModuleNames().Count() == JAM_MODULE_COUNT) { // Precise Instability
+            int validCount = 0;
+
+            foreach (string module in JAM_MODULE_NAMES) {
+                if (Bomb.GetSolvableModuleNames().Count(x => x.Contains(module)) >= 1)
+                    validCount++;
+            }
+
+            if (validCount == JAM_MODULE_COUNT - 1) {
+                Debug.LogFormat("[Mission Control #{0}] Found mission: \"Precise Instability\"", moduleId);
+                missionFound = true;
+                mode = 5;
+                StartCoroutine(HideJamModules());
+                storedNumber = UnityEngine.Random.Range(1, 21);
+            }
         }
     }
 
@@ -178,10 +236,39 @@ public class MissionControl : MonoBehaviour {
         yield return null;
     }
 
+    // Finds and hides modules for Precise Instability
+    private IEnumerator HideJamModules() {
+        canPressButton = false;
+        int moduleCounter = 0;
+        for (int i = 0; i < transform.parent.childCount; i++) {
+            var module = transform.parent.GetChild(i).gameObject.GetComponent<KMBombModule>();
+            if (module == null)
+                continue;
+
+            if (moduleCounter < jamModule.Length) {
+                foreach (string name in JAM_MODULES) {
+                    if (module.ModuleType == name) {
+                        jamModule[moduleCounter] = module;
+                        StartCoroutine(HideJamModule(moduleCounter, false, false));
+                        moduleCounter++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        Debug.LogFormat("<Mission Control #{0}> Hiding module: Mission Control", moduleId);
+        missionControlScale = Module.transform.localScale;
+        Module.transform.localScale = new Vector3(0, 0, 0);
+        readyToChange = true;
+        yield return null;
+    }
+
+
     // Covers a module for Wish
     private IEnumerator CoverModule(int num) {
         // Code from Mystery Module
-        Debug.LogFormat(@"[Mission Control #{0}] Hiding module: {1}", moduleId, mystifiedModule[num].ModuleDisplayName);
+        Debug.LogFormat("[Mission Control #{0}] Hiding module: {1}", moduleId, mystifiedModule[num].ModuleDisplayName);
 
         Cover[num].SetActive(true);
         var mysPos = mystifiedModule[num].transform.localPosition;
@@ -194,7 +281,7 @@ public class MissionControl : MonoBehaviour {
             Cover[num].transform.localPosition = new Vector3(mysPos.x, mysPos.y - 0.02f, mysPos.z);
         else
             Cover[num].transform.localPosition = new Vector3(mysPos.x, mysPos.y + 0.02f, mysPos.z);
-        Debug.LogFormat(@"<Mission Control #{0}> Rotation: {1}", moduleId, Cover[num].transform.rotation);
+        Debug.LogFormat("<Mission Control #{0}> Rotation: {1}", moduleId, Cover[num].transform.rotation);
         yield return null;
         Cover[num].transform.parent = transform.parent;
 
@@ -215,7 +302,7 @@ public class MissionControl : MonoBehaviour {
     // Reveals a module for Wish
     private IEnumerator RevealWishModule(int num) {
         // Code from Mystery Module
-        Debug.LogFormat(@"[Mission Control #{0}] Revealing module: {1}", moduleId, mystifiedModule[num].ModuleDisplayName);
+        Debug.LogFormat("[Mission Control #{0}] Revealing module: {1}", moduleId, mystifiedModule[num].ModuleDisplayName);
 
         /*MethodInfo mth;
         foreach (var component in mystifiedModule[num].gameObject.GetComponents<MonoBehaviour>())
@@ -233,6 +320,54 @@ public class MissionControl : MonoBehaviour {
         }
         mystifiedModule[num].transform.localScale = mystifyScale[num];
         Destroy(Cover[num]);
+        yield return null;
+    }
+
+
+    // Hides a module for Precise Instability
+    private IEnumerator HideJamModule(int num, bool delay, bool validate) {
+        Debug.LogFormat("<Mission Control #{0}> Hiding module: {1}", moduleId, jamModule[num].ModuleDisplayName);
+
+        if (!delay) {
+            jamModuleScale[num] = jamModule[num].transform.localScale;
+            jamModule[num].transform.localScale = new Vector3(0, 0, 0);
+        }
+
+        else {
+            var duration = 0.5f;
+            var elapsed = 0.0f;
+            while (elapsed < duration) {
+                yield return null;
+                elapsed += Time.deltaTime;
+                jamModule[num].transform.localScale = Vector3.Lerp(jamModuleScale[num], new Vector3(0.0f, 0.0f, 0.0f), elapsed / duration);
+            }
+
+            jamModule[num].transform.localScale = new Vector3(0, 0, 0);
+        }
+
+        if (validate)
+            readyToChange = true;
+
+        yield return null;
+    }
+
+    // Reveals a module for Precise Instability
+    private IEnumerator RevealJamModule(int num, bool validate) {
+        Debug.LogFormat("<Mission Control #{0}> Revealing module: {1}", moduleId, jamModule[num].ModuleDisplayName);
+
+        var duration = 2.0f;
+        var elapsed = 0.0f;
+        while (elapsed < duration) {
+            yield return null;
+            elapsed += Time.deltaTime;
+            jamModule[num].transform.localScale = Vector3.Lerp(new Vector3(0.0f, 0.0f, 0.0f), jamModuleScale[num], elapsed / duration);
+        }
+
+        jamModule[num].transform.localScale = jamModuleScale[num];
+
+        if (validate)
+            readyToChange = true;
+
         yield return null;
     }
 
@@ -319,9 +454,207 @@ public class MissionControl : MonoBehaviour {
                 }
             }
             break;
+
+        case 5: // Precise Instability
+            if (acceptingStrikes && readyToChange && bombStrikes != Bomb.GetStrikes()) {
+                actualStrikes++;
+
+                // Bomb has 4+ strikes
+                if (actualStrikes >= 4 && !ZenModeActive && !TimeModeActive) {
+                    Debug.LogFormat("[Mission Control #{0}] Strike limit reached! Detonating bomb.", moduleId);
+                    while (Bomb.GetStrikes() < 4)
+                        Module.HandleStrike();
+                }
+
+                Debug.LogFormat("[Mission Control #{0}] Strike detected on another module! Entering countdown mode.", moduleId);
+                Debug.LogFormat("[Mission Control #{0}] To remove the effects of the strike, press the button when the timer displays {1}.", moduleId, storedNumber);
+
+                freezeTimer = true;
+                StartCoroutine(FreezeTimer());
+                storedTime = Bomb.GetTime();
+
+                acceptingStrikes = false;
+                readyToChange = false;
+                flickerText = false;
+                franticMode = true;
+                enteredTimerNumber = false;
+
+                for (int i = 0; i < jamModule.Length; i++) {
+                    if (i == jamModule.Length - 1)
+                        StartCoroutine(HideJamModule(i, true, true));
+                    
+                    else
+                        StartCoroutine(HideJamModule(i, true, false));
+                }
+
+                StartCoroutine(ChangeBorderColor(false));
+                StartCoroutine(StartTimer());
+            }
+            break;
         }
     }
 
+
+    // Initiates the bomb for Precise Instability
+    private IEnumerator InitJamBomb() {
+        while (!readyToChange) { }
+        readyToChange = false;
+        StartCoroutine(AnimateTimer());
+
+        for (int i = 0; i < jamModule.Length; i++) {
+            StartCoroutine(RevealJamModule(i, false));
+            yield return new WaitForSeconds(0.07f);
+        }
+
+        Debug.LogFormat(@"<Mission Control #{0}> Revealing module: Mission Control", moduleId);
+        var duration = 2.0f;
+        var elapsed = 0.0f;
+        while (elapsed < duration) {
+            yield return null;
+            elapsed += Time.deltaTime;
+            Module.transform.localScale = Vector3.Lerp(new Vector3(0.0f, 0.0f, 0.0f), missionControlScale, elapsed / duration);
+        }
+
+        Module.transform.localScale = missionControlScale;
+        readyToChange = true;
+        canPressButton = true;
+        yield return null;
+    }
+
+    // Animates the timer for Precise Instability
+    private IEnumerator AnimateTimer() {
+        float delayTime = 1.0f / 60.0f;
+        float startTime = 120.0f;
+        float endTime = JAM_BOMB_TIME + storedNumber;
+
+        if (ZenModeActive) {
+            startTime = 0.0f;
+            endTime = storedNumber;
+        }
+
+        else if (TimeModeActive) {
+            endTime = 600.0f + storedNumber;
+        }
+
+        yield return new WaitForSeconds(2.0f);
+
+        for (float i = 0.0f; i < 105.0f; i++) {
+            TimeRemaining.FromModule(Module, (endTime - startTime) * (i / 105.0f) + startTime);
+            yield return new WaitForSeconds(delayTime);
+        }
+
+        TimeRemaining.FromModule(Module, endTime + 0.04f);
+
+        freezeTimer = true;
+        StartCoroutine(FreezeTimer());
+
+        yield return new WaitForSeconds(2.0f);
+        freezeTimer = false;
+        acceptingStrikes = true;
+        yield return null;
+    }
+
+    // Freezes the timer in place
+    private IEnumerator FreezeTimer() {
+        float frozenTime = Bomb.GetTime();
+        while (freezeTimer) {
+            TimeRemaining.FromModule(Module, frozenTime);
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        yield return null;
+    }
+
+
+    // Changes the color of the button and border
+    private IEnumerator ChangeBorderColor(bool direction) {
+        float borderGreen = 0.6640625f;
+        float buttonGreen = 0.75f;
+        float buttonBlue = 0.5f;
+
+        if (!direction) {
+            for (float i = 89.0f; i >= 0.0f; i--) {
+                BorderMaterial.color = new Color(1.0f, borderGreen * (i / 90.0f), 0.0f);
+                PlanetMaterial.color = new Color(1.0f, buttonGreen * (i / 90.0f), buttonBlue * (i / 90.0f));
+                yield return new WaitForSeconds(0.02f);
+            }
+
+            BorderMaterial.color = new Color(1.0f, 0.0f, 0.0f);
+            PlanetMaterial.color = new Color(1.0f, 0.0f, 0.0f);
+        }
+
+        else {
+            for (float i = 0.0f; i < 90.0f; i++) {
+                BorderMaterial.color = new Color(1.0f, borderGreen * (i / 90.0f), 0.0f);
+                PlanetMaterial.color = new Color(1.0f, buttonGreen * (i / 90.0f), buttonBlue * (i / 90.0f));
+                yield return new WaitForSeconds(0.02f);
+            }
+
+            BorderMaterial.color = new Color(1.0f, borderGreen, 0.0f);
+            PlanetMaterial.color = new Color(1.0f, buttonGreen, buttonBlue);
+            acceptingStrikes = true;
+        }
+
+        yield return null;
+    }
+
+    // Starts the countdown timer
+    private IEnumerator StartTimer() {
+        Audio.PlaySoundAtTransform("missionControl_MechanismsClock", transform);
+        ButtonText.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+        for (displayedSecond = 30; displayedSecond > 0; displayedSecond--) {
+            ButtonText.text = displayedSecond.ToString();
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        ButtonText.text = "0";
+        if (enteredSecond == storedNumber) {
+            Audio.PlaySoundAtTransform("missionControl_goodChime", transform);
+            Debug.LogFormat("[Mission Control #{0}] You pressed at the correct time! One strike removed!", moduleId);
+
+            // Code by Emik
+            var bomb = GetComponent<KMBomb>();
+            int strikes = bomb.GetStrikes();
+            strikes -= 1;
+            bomb.SetStrikes(strikes);
+        }
+
+        else {
+            Audio.PlaySoundAtTransform("missionControl_badChime", transform);
+            Debug.LogFormat("[Mission Control #{0}] You pressed the button at the wrong time.", moduleId);
+            bombStrikes++;
+        }
+
+        storedNumber = ((int) storedTime) % 20;
+        storedNumber = storedNumber == 0 ? 20 : storedNumber;
+        Debug.LogFormat("[Mission Control #{0}] The number to press for the next countdown is {1}.", moduleId, storedNumber);
+
+        yield return new WaitForSeconds(2.0f);
+        freezeTimer = false;
+        ButtonText.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        ButtonText.text = actualStrikes.ToString();
+        Debug.LogFormat("[Mission Control #{0}] The bomb currently has {1} internal strikes.", moduleId, actualStrikes);
+        
+        flickerText = true;
+        transparency = 0.0f;
+        StartCoroutine(FlickerTextRoutine());
+
+        franticMode = false;
+        StartCoroutine(ChangeBorderColor(true));
+
+        while (!readyToChange) { }
+
+        for (int i = 0; i < jamModule.Length; i++) {
+            if (i == jamModule.Length - 1)
+                StartCoroutine(RevealJamModule(i, true));
+
+            else
+                StartCoroutine(RevealJamModule(i, false));
+        }
+
+        yield return null;
+    }
 
     // Lights turn on
     private void OnActivate() {
@@ -330,6 +663,9 @@ public class MissionControl : MonoBehaviour {
                 canPlayIntro = false;
                 Audio.PlaySoundAtTransform("missionControl_inEffect", transform);
             }
+
+            if (mode == 5)
+                StartCoroutine(InitJamBomb());
         }
 
         else {
@@ -358,7 +694,11 @@ public class MissionControl : MonoBehaviour {
     // Rotates the button so the texture animates
     private IEnumerator AnimateButton() {
         while (true) {
-            ButtonTransform.Rotate(new Vector3(0.05f, 0.0f, 0.0f));
+            if (franticMode)
+                ButtonTransform.Rotate(new Vector3(0.25f, 0.0f, 0.0f));
+
+            else
+                ButtonTransform.Rotate(new Vector3(0.05f, 0.0f, 0.0f));
             yield return new WaitForSeconds(0.02f);
         }
     }
@@ -378,6 +718,8 @@ public class MissionControl : MonoBehaviour {
 
             yield return new WaitForSeconds(0.02f);
         }
+
+        ButtonText.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
     }
 
 
@@ -400,8 +742,15 @@ public class MissionControl : MonoBehaviour {
     private void ButtonPressed() {
         ButtonSelectable.AddInteractionPunch(0.5f);
 
+        if (franticMode && !enteredTimerNumber) {
+            enteredSecond = displayedSecond;
+            enteredTimerNumber = false;
+            Audio.PlaySoundAtTransform("missionControl_buttonPress", transform);
+            Debug.LogFormat("[Mission Control #{0}] You pressed the button when the timer displayed {1}.", moduleId, enteredSecond);
+        }
+
         // Wish
-        if (mode == 4 && canPressButton && !moduleSolved) {
+        else if (mode == 4 && canPressButton && !moduleSolved) {
             canPressButton = false;
             Audio.PlaySoundAtTransform("missionControl_buttonPress", transform);
             Debug.LogFormat("[Mission Control #{0}] You pressed the button. Total presses: {1}", moduleId, buttonPresses + 1);
@@ -453,6 +802,7 @@ public class MissionControl : MonoBehaviour {
     // Variable set by Tweaks for Zen mode detection
     #pragma warning disable 414
     private bool ZenModeActive;
+    private bool TimeModeActive;
     #pragma warning restore 414
 
     // Twitch Plays command handler - by eXish
